04398f65978184f811caadba5bfaebd66f0b510c
unknown <oluwatobiloba8624@gmail.com>
2022-05-15 19:07:25 +0100

    designing static

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+
+import Button from '../Button/Button';
+
+const Header = (props) => {
+    return (
+        <div className='items-center text-center bg-[#E9ECFF] grid grid-rows-1 md:grid-cols-2 md:grid-cols-2 py-10 px-[20px] md:px-[32px] xl:px-[72px] h-fit'>
+            <div className=' xl:pr-28 md:text-left '>
+                <h1 className="text-3xl md:text-3xl xl:text-5xl text-gray-900 font-[700] py-2 tracking-tight ">Discover the world's top designers & creatives</h1>
+                <p className="py-2 mb-2 leading-relaxed">Dribbble is the leading destination to find & showcase creative work and home to the world's
+                   best design professionals
+                </p>
+
+                <Button title={"Sign up"} />
+            </div>      
+            <div className='row-start-1 md:col-start-2'>
+                <img className="inline-block mb-4 w-1/2 md:w-5/6 xl:w-8/12 xl:self-end" alt="Dribbble is the leading destination to find &amp; showcase creative work and home to the world's best design professionals." src="https://cdn.dribbble.com/assets/art-banners/mergedevt-600w-57271ebfdf4f44412491e6ac02d1dfdc2b2631f553e4c462db4776c94ca2ce0b.png" />
+                <p className="text-slate-400 text-[14px] xl:self-end md:text-right xl:text-center"><span className='link'>Learn</span> more about how we are supporting Ukraine. Art by <span className="link">Merge Development</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,23 @@
 import Navigation from '../components/Navigation/Navigation';
+import Header from '../components/Header/Header';
+import Button from '../components/Button/Button';
+import Footer from '../components/Footer/Footer';
 
 function App() {
   return (
     <main>
-      <div className=''>
+      <div>
         
         <Navigation />
+
+        <Header />
+
+        <div className="flex flex-row justify-center items-center gap-16 py-[40px]">
+          <Button title={"Sign up to continue"}/> <span className="text-custom-pink cursor-pointer hover:opacity-70">or sign in</span>
+        </div>
+        
+
+        <Footer />
       </div>
     </main>
   );
